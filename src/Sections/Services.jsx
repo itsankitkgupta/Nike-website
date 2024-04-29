909fc73e09fcc46da225515fdad8e21c41142f06
itsankitkgupta <ankitgupta132962@gmail.com>
2024-04-29 17:40:35 +0530

    FrontendComplete

---
+++ src/Sections/Services.jsx
@@ -0,0 +1,14 @@
+import { services } from "../Constants/Index";
+import { ServiceCard } from "../Components/Index";
+
+const Services = () => {
+  return (
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
+
+export default Services;
